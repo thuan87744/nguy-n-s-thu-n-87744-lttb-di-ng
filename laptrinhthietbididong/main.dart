dad6118840d2d2862cafde36d46d19a612e82647
thuan87744 <127909302+thuan87744@users.noreply.github.com>
2023-04-05 09:57:07 +0700

    Add files via upload

---
+++ laptrinhthietbididong/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main(){
+  print('Hello World');
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World')
+        )
+      )
+    )
+  );
+}
